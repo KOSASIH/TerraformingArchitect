e70683de4a1372fa8262d389894e4055173cc8de
KOSASIH <kosasihg88@gmail.com>
2023-10-10 11:22:27 +0700

    Create embed_code.py

---
+++ embed_code.py
@@ -0,0 +1,4 @@
+To embed a 3D model of a planet or celestial body in a markdown document, you can use the following code snippet:
+
+```html
+<iframe src="https://your-3d-model-website.com/your-model" width="800" height="600"></iframe>
